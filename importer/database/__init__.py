dd16ff98d55adbbf957d562a2ce08ab72a8fd93e
Ifigenia Poulka <ifigeniapoulka@redmi-note-13-pro-5g.home>
2026-01-13 10:04:58 +0100

    importer completed

--- importer/database/__init__.py
+++ importer/database/__init__.py
@@ -19,3 +19,181 @@ SQL_INSERT_TRANSLATION = """
         description = VALUES(description),
         comment = VALUES(comment)
 """
+
+SQL_INSERT_DATE_RULE_TYPE = """
+    INSERT INTO date_rule_type(
+        id,
+        code,
+        name,
+        name_translation_id,
+        description,
+        description_translation_id,
+        comment,
+        comment_translation_id
+    )
+    VALUES (%s, %s, %s, %s, %s, %s,%s,%s)
+    ON DUPLICATE KEY UPDATE
+        code = VALUES(code),
+        name = VALUES(name),
+        name_translation_id = VALUES(name_translation_id),
+        description = VALUES(description),
+        description_translation_id = VALUES(description_translation_id),
+        comment = VALUES(comment),
+        comment_translation_id = VALUES(comment_translation_id)
+"""
+
+SQL_INSERT_FASTING_TYPE = """
+    INSERT INTO fasting_type(
+        id,
+        code,
+        name,
+        name_translation_id,
+        description,
+        description_translation_id,
+        comment,
+        comment_translation_id,
+        meat,
+        dairy,
+        fish,
+        wineoil
+    )
+    VALUES (%s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s)
+    ON DUPLICATE KEY UPDATE
+        code = VALUES(code),
+        name = VALUES(name),
+        name_translation_id = VALUES(name_translation_id),
+        description = VALUES(description),
+        description_translation_id = VALUES(description_translation_id),
+        comment = VALUES(comment),
+        comment_translation_id = VALUES(comment_translation_id),
+        meat = VALUES(meat),
+        dairy = VALUES(dairy),
+        fish = VALUES(fish),
+        wineoil = VALUES(wineoil)
+"""
+
+SQL_INSERT_EVENT_CATEGORY = """
+    INSERT INTO event_category(
+        id,
+        parent_category_id,
+        code,
+        name,
+        name_translation_id,
+        description,
+        description_translation_id,
+        comment,
+        comment_translation_id
+    )
+    VALUES (%s, %s, %s, %s, %s, %s, %s,%s,%s)
+    ON DUPLICATE KEY UPDATE
+       parent_category_id = VALUES(parent_category_id),
+       code = VALUES(code),
+       name = VALUES(name),
+       name_translation_id = VALUES(name_translation_id),
+       description = VALUES(description),
+       description_translation_id = VALUES(description_translation_id),
+       comment = VALUES(comment),
+       comment_translation_id = VALUES(comment_translation_id)
+"""
+
+SQL_INSERT_EVENT= """
+    INSERT INTO event(
+        id,
+        code,
+        is_season,
+        event_category_id,
+        name,
+        name_translation_id,
+        description,
+        description_translation_id,
+        comment,
+        comment_translation_id
+    )
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    ON DUPLICATE KEY UPDATE
+        code = VALUES(code),
+        is_season = VALUES(is_season),
+        event_category_id=VALUES(event_category_id),
+        name= VALUES(name),
+        name_translation_id=VALUES(name_translation_id),
+        description=VALUES(description),
+        description_translation_id=VALUES(description_translation_id),
+        comment=VALUES(comment),
+        comment_translation_id=VALUES(comment_translation_id)
+"""
+
+SQL_INSERT_EVENT_DATE_RULE= """
+    INSERT INTO event_date_rule(
+        id,
+        code,
+        date_rule_type_id,
+        fasting_type_id,
+        event_id,
+        start_month,
+        start_day,
+        end_month,
+        end_day,
+        description,
+        description_translation_id,
+        comment,
+        comment_translation_id
+     )
+    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    ON DUPLICATE KEY UPDATE
+        code=VALUES(code),
+        date_rule_type_id=VALUES(date_rule_type_id),
+        fasting_type_id=VALUES(fasting_type_id),
+        event_id=VALUES(event_id),
+        start_month=VALUES(start_month),
+        start_day=VALUES(start_day),
+        end_month=VALUES(end_month),
+        end_day=VALUES(end_day),
+        description=VALUES(description),
+        description_translation_id=VALUES(description_translation_id),
+        comment= VALUES(comment),
+        comment_translation_id=VALUES(comment_translation_id)
+ """
+
+SQL_INSERT_LANGUAGE= """
+    INSERT INTO language(
+        id,
+        code,
+        name,
+        name_translation_id,
+        description,
+        description_translation_id,
+        comment,
+        comment_translation_id
+        )
+    VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+    ON DUPLICATE KEY UPDATE
+        code=VALUES(code),
+        name=VALUES(name),
+        name_translation_id=VALUES(name_translation_id),
+        description=VALUES(description),
+        description_translation_id=VALUES(description_translation_id),
+        comment=VALUES(comment),
+        comment_translation_id=VALUES(comment_translation_id)         
+ """
+
+SQL_INSERT_DOCUMENT_CATEGORY= """
+    INSERT INTO document_category(
+        id,
+        code,
+        name,
+        name_translation_id,
+        description,
+        description_translation_id,
+        comment,
+        comment_translation_id
+     )
+    VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+    ON DUPLICATE KEY UPDATE
+        code=VALUES(code),
+        name=VALUES(name),
+        name_translation_id=VALUES(name_translation_id),
+        description=VALUES(description),
+        description_translation_id=VALUES(description_translation_id),
+        comment=VALUES(comment),
+        comment_translation_id=VALUES(comment_translation_id)
+ """
--- importer/importer.py
+++ importer/importer.py
@@ -3,14 +3,15 @@ import mysql.connector as mysql
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-from database import SQL_INSERT_TRANSLATION
+from database import SQL_INSERT_DATE_RULE_TYPE,SQL_INSERT_DOCUMENT_CATEGORY, SQL_INSERT_EVENT, SQL_INSERT_EVENT_DATE_RULE, SQL_INSERT_LANGUAGE, SQL_INSERT_TRANSLATION,SQL_INSERT_FASTING_TYPE ,SQL_INSERT_EVENT_CATEGORY
+
 
 # Download and install mySQL dirver:
-# 1. check which environment importer.py is using with command:which python OR
+# 1. check which environment importer.py is using with command:which python3 OR
 #    check bottom right hand status bar.
-# 2. check whether the MySQL droiver is install in THAT environment with : python -m pip show mysql-connector-python
-#     if not installed run: python -m pip install mysql-connector-python
-# 3. Verify that it is installed with: python -m pip show mysql-connector-python
+# 2. check whether the MySQL driver is installed in THAT environment with : python3 -m pip show mysql-connector-python
+#     if not installed run: python3 -m pip install mysql-connector-python
+# 3. Verify that it is installed with: python3 -m pip show mysql-connector-python
 # Note: pip is Python's package installer
 
 # validation helper
@@ -20,8 +21,6 @@ def validate_loads(csv_path):
     pass
 
 # translation
-
-
 def translation_load(csv_path, cur):
     csv_path = Path(csv_path)
 
@@ -33,14 +32,55 @@ def translation_load(csv_path, cur):
             data_reader = csv.reader(file)
             next(data_reader)
 
-            for row in data_reader:
+            for row in data_reader:   
                 cur.execute(SQL_INSERT_TRANSLATION, row)
     except mysql.errors.IntegrityError as e:
         print(str(e))
 
+# date_rule_type
+def date_rule_type_load(csv_path, cur):
+    csv_path = Path(csv_path)
+
+    if not csv_path.exists() or not csv_path.is_file():
+        raise ValueError("Does not exist or is not a file")
+
+    try:
+        with csv_path.open("r", encoding="utf-8-sig", newline="") as file:
+            data_reader = csv.reader(file)
+            next(data_reader)
+
+            for row in data_reader:
+                if row[3] in ("", "NULL", "null"):
+                    row[3] = None
+                else:
+                    row[3] = int(row[3])
+                cur.execute(SQL_INSERT_DATE_RULE_TYPE, row)
+    except mysql.errors.IntegrityError as e:
+        print(str(e))
+
+# fasting type
+def fasting_type_load(csv_path, cur):
+    csv_path = Path(csv_path)
+
+    if not csv_path.exists() or not csv_path.is_file():
+        raise ValueError("Does not exist or is not a file")
+
+    try:
+        with csv_path.open("r", encoding="utf-8-sig", newline="") as file:
+            data_reader = csv.reader(file)
+            next(data_reader)
+
+            for row in data_reader:
+                if row[3] in ("", "NULL", "null"):
+                    row[3] = None
+                else:
+                    row[3] = int(row[3])
+
+                cur.execute(SQL_INSERT_FASTING_TYPE, row)
+    except mysql.errors.IntegrityError as e:
+        print(str(e))
+
 # event_category
-
-
 def event_category_load(csv_path, cur):
     csv_path = Path(csv_path)
 
@@ -58,13 +98,11 @@ def event_category_load(csv_path, cur):
                 else:
                     row[1] = int(row[1])
 
-                cur.execute("INSERT INTO event_category(id, parent_category_id, code, name, name_translation_id,description,description_translation_id,comment,comment_translation_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)", row)
+                cur.execute(SQL_INSERT_EVENT_CATEGORY, row)
     except mysql.errors.IntegrityError as e:
         print(str(e))
 
 # event
-
-
 def event_load(csv_path, cur):
     csv_path = Path(csv_path)
 
@@ -79,14 +117,129 @@ def event_load(csv_path, cur):
             category_id = ""
             name_translation = ""
             for row in data_reader:
+                if row[3] in ("", "NULL", "null"):
+                    row[3] = None
+                else:
+                    row[3] = int(row[3])
                 event_id = row[0]
                 category_id = row[3]
                 name_translation = row[5]
-                cur.execute("INSERT INTO event(id, code, is_season, event_category_id, name, name_translation_id, description, description_translation_id, comment, comment_translation_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", row)
+                cur.execute(SQL_INSERT_EVENT, row)
     except mysql.errors.IntegrityError as e:
         print(
             f"foreign key missing: event: {event_id} and category id:{category_id} and translation id:{name_translation}" + str(e))
 
+# event_date_rule
+def event_date_rule_load(csv_path, cur):
+    csv_path = Path(csv_path)
+
+    if not csv_path.exists() or not csv_path.is_file():
+        raise ValueError("Does not exist or is not a file")
+
+    
+    with csv_path.open("r", encoding="utf-8-sig", newline="") as file:
+            data_reader = csv.reader(file)
+            next(data_reader)
+
+            for row in data_reader:
+                if row[3] in ("", "NULL", "null"):
+                    row[3] = None
+                else:
+                    row[3] = int(row[3])
+
+                if row[5] in ("", "NULL", "null"):
+                    row[5] = None
+                else:
+                    row[5] = int(row[5])
+
+                if row[6] in ("", "NULL", "null"):
+                    row[6] = None
+                else:
+                    row[6] = int(row[6])
+
+                if row[7] in ("", "NULL", "null"):
+                    row[7] = None
+                else:
+                    row[7] = int(row[7])
+
+                if row[8] in ("", "NULL", "null"):
+                    row[8] = None
+                else:
+                    row[8] = int(row[8]) 
+
+                if row[2] in ("", "NULL", "null"):
+                    row[2] = None
+                else:
+                    row[2] = int(row[2])    
+
+                if row[4] in ("", "NULL", "null"):
+                    row[4] = None
+                else:
+                    row[4] = int(row[4])   
+
+                if row[10] in ("", "NULL", "null"):
+                    row[10] = None
+                else:
+                    row[10] = int(row[10])  
+
+                if row[12] in ("", "NULL", "null"):
+                    row[12] = None
+                else:
+                    row[12] = int(row[12])   
+               
+                try:
+                    cur.execute(SQL_INSERT_EVENT_DATE_RULE, row)
+                except mysql.Error as e:
+                    print(f"Failed row id={row[0]}: {e}")
+                    continue
+    
+      
+        # optionally continue to the next row
+           
+
+# language
+def language_load(csv_path, cur):
+    csv_path = Path(csv_path)
+
+    if not csv_path.exists() or not csv_path.is_file():
+        raise ValueError("Does not exist or is not a file")
+
+    try:
+        with csv_path.open("r", encoding="utf-8-sig", newline="") as file:
+            data_reader = csv.reader(file)
+            next(data_reader)
+
+            for row in data_reader:
+                if row[3] in ("", "NULL", "null"):
+                    row[3] = None
+                else:
+                    row[3] = int(row[3])
+
+                cur.execute(SQL_INSERT_LANGUAGE, row)
+    except mysql.errors.IntegrityError as e:
+        print(str(e))
+
+# document_category
+def document_category_load(csv_path, cur):
+    csv_path = Path(csv_path)
+
+    if not csv_path.exists() or not csv_path.is_file():
+        raise ValueError("Does not exist or is not a file")
+
+    try:
+        with csv_path.open("r", encoding="utf-8-sig", newline="") as file:
+            data_reader = csv.reader(file)
+            next(data_reader)
+
+            for row in data_reader:
+                if row[3] in ("", "NULL", "null"):
+                    row[3] = None
+                else:
+                    row[3] = int(row[3])
+
+                cur.execute(SQL_INSERT_DOCUMENT_CATEGORY, row)
+    except mysql.errors.IntegrityError as e:
+        print(str(e))        
 
 # cur.execute("SELECT VERSION()")
 
@@ -141,29 +294,45 @@ def main():
     # cursor executes SQL and fetches results via the connection
     cur = mydb.cursor()
 
+
     load_translation = csv_path + os.getenv("TRANSLATION_CSV")
+    load_date_rule_type = csv_path + os.getenv("DATE_RULE_TYPE_CSV")
+    load_fasting_type = csv_path + os.getenv("FASTING_TYPE_CSV")
     load_event_category = csv_path + os.getenv("EVENT_CATEGORY_CSV")
     load_event = csv_path + os.getenv("EVENT_CSV")
+    load_event_date_rule_type= csv_path + os.getenv("EVENT_DATE_RULE_CSV")
+    load_language = csv_path + os.getenv("LANGUAGE_CSV")
+    load_document_category = csv_path + os.getenv("DOCUMENT_CATEGORY_CSV")
 
+    data_paths = {load_translation:translation_load,
+                  load_date_rule_type:date_rule_type_load,
+                  load_fasting_type:fasting_type_load,
+                  load_event_category:event_category_load,
+                  load_event:event_load,
+                  load_event_date_rule_type:event_date_rule_load,
+                  load_language:language_load,
+                  load_document_category:document_category_load
+                  }
+    
     # Verify if each key for each csv exist or is not empty. Then call the def for that particular path.
-    # if key does not exist, ask user to input the path.NOTE:User might add rubbish.
-    if load_event:
-        translation_load(load_translation, cur)
-        event_category_load(load_event_category, cur)
-        # event_load(load_event, cur)
-    else:
-        print("Key does not exist")
+    # if key does not exist, ask user to input the path.NOTE:User might add nonesense.
+    
+    for load_path, load_function in data_paths.items():
+        if load_path:
+            load_function(load_path, cur)
+        else:
+            print(f"Key for {load_function.__name__} does not exist")
 
-        try:
-            inputfile_path = input("Enter the file path for event.csv")
-            inputfile_path = inputfile_path.strip()
+            try:
+                inputfile_path = input("Enter the file path for event.csv")
+                inputfile_path = inputfile_path.strip()
 
-            if not inputfile_path:
-                print("You have entered an empty String")
-            else:
-                event_load(inputfile_path, cur)
-        except ValueError as e:
-            print("You have entered an invalid path. It should be in the following format"
+                if not inputfile_path:
+                    print("You have entered an empty String")
+                else:
+                    load_function(inputfile_path, cur)
+            except ValueError as e:
+                print("You have entered an invalid path. It should be in the following format"
                   "/Users/<name>/folder/....csv"+str(e))
 
     mydb.commit()
